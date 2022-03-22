0924aab31a87f2944883250c1af40af74fad41a0
rubik16x16 <drt_mike@hotmail.com>
2022-03-22 11:22:04 -0400

    Change deploy port

--- app/Http/Controllers/Home.php
+++ app/Http/Controllers/Home.php
@@ -8,17 +8,6 @@ class Home extends Controller{
 
 	public function index(){
 
-		$stripe = new \Stripe\StripeClient(
-		  env('STRIPE_SECRET_KEY')
-		);
-
-		$payment = $stripe->paymentIntents->retrieve(
-		  'pi_3KfuD9CzfGR8PWZf1iaoKlOG',
-		  []
-		);
-
-		dd($payment);
-
 		return view('index');
 	}
 }
