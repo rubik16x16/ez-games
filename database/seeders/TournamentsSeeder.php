1e077f80562b2b003750accd4c77a9d321e458d8
Anthony <drt_mike@hotmail.com>
2022-05-24 12:23:13 -0400

    Change tournament seeder

--- database/seeders/TournamentsSeeder.php
+++ database/seeders/TournamentsSeeder.php
@@ -16,29 +16,29 @@ class TournamentsSeeder extends Seeder{
 		Tournament::insert([
 			[
 				'name' => 'BR SOLO Rebirth Solo',
-				'start' => '2022-03-27 12:00:00',
+				'start' => '2022-06-24 12:00:00',
 				'end' => '2022-03-27 14:00:00',
-				'image' => 'storage/images/tournaments/tournaments-games-1.png',
+				'image' => 'storage/images/tournaments/Tournament-Cards-1.png',
 				'num_players' => 1,
-				'entry' => null,
+				'entry' => 100,
 				'reward' => 30
 			],
 			[
 				'name' => 'BR DUOS Rebirth Duos',
-				'start' => '2022-03-27 12:00:00',
+				'start' => '2022-06-25 12:00:00',
 				'end' => '2022-03-27 14:00:00',
-				'image' => 'storage/images/tournaments/tournaments-games-2.png',
+				'image' => 'storage/images/tournaments/Tournament-Cards-2.png',
 				'num_players' => 2,
-				'entry' => null,
+				'entry' => 200,
 				'reward' => 30
 			],
 			[
 				'name' => 'BR TRIOS Rebirth Trios',
-				'start' => '2022-03-27 12:00:00',
+				'start' => '2022-06-26 12:00:00',
 				'end' => '2022-03-27 14:00:00',
-				'image' => 'storage/images/tournaments/tournaments-games-3.png',
+				'image' => 'storage/images/tournaments/Tournament-Cards-1.png',
 				'num_players' => 3,
-				'entry' => null,
+				'entry' => 300,
 				'reward' => 30
 			]
 		]);
